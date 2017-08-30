9859520003eb06fa940677030e8ae8f114a2c794
SeanPesce <pesce.sean@gmail.com>
2017-08-30 00:37:51 -0400

    Add resource files for Windows mouse cursor

---
+++ include/Resource.h
@@ -0,0 +1,20 @@
+// Author: Sean Pesce
+
+
+#pragma once
+
+#ifndef _SP_D3D9O_RESOURCE_H_
+	#define _SP_D3D9O_RESOURCE_H_
+
+
+
+#ifndef IDC_STATIC
+	#define IDC_STATIC (-1)
+#endif // IDC_STATIC
+
+
+#define ID_NUM_WIN_CURSOR_PNG 451
+
+
+#endif // _SP_D3D9O_RESOURCE_H_
+
